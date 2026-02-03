c43cf2e68d314a0e5ca0a21f69cc82b6040c8c0c
VishupHell <toptoros06@gmail.com>
2026-02-03 12:36:46 +0300

    finish version

--- hw4/server.py
+++ hw4/server.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
+
 from dotenv import dotenv_values
 
+from controllers import operation
+
 app = Flask(__name__)
+
+@app.route("/")
+def server_info() -> str:
+    return "My server"
 
 @app.route("/author")
 def author():
@@ -13,11 +21,13 @@ def author():
     }
     return jsonify(author)
 
-@app.route("/")
-def server_info():
-    return "My first flask server"
+@app.route("/sum")
+def runner():
+    a = request.args.get('a', type=int)
+    b = request.args.get('b', type=int)
+    return jsonify({'sum': operation(a, b)})
 
-def get_port():
+def get_port() -> int:
     config = dotenv_values(".env")
     if "PORT" in config:
         return config["PORT"]
