cd5c5ffb23af99f3611945b53df34762dba70a76
VishupHell <toptoros06@gmail.com>
2026-03-01 20:33:35 +0300

    update project

--- hw5/project/urls.py
+++ hw5/project/urls.py
@@ -15,12 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
+from rest_framework import routers
+
+from backend.views import UsersAPIViewSet, PostsAPIViewSet, CommentsAPIViewSet
+
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+router = routers.DefaultRouter()
+router.register(r'users', UsersAPIViewSet)
+router.register(r'posts', PostsAPIViewSet)
+router.register(r'users', CommentsAPIViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/', include(router.urls))
 ]
